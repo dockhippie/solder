3c7905dc879a927f334cb7293e0d635259c56401
Thomas Boerger <thomas@webhippie.de>
2022-08-31 10:44:39 +0200

    feat: add env variable to disable mod api

--- v0.7/overlay/srv/www/config/solder.php
+++ v0.7/overlay/srv/www/config/solder.php
@@ -7,4 +7,5 @@ return [
     'mirror_url' => Str::finish(env('SOLDER_MIRROR_URL'), '/'),
     'md5_connect_timeout' => intval(env('SOLDER_MD5_CONNECT_TIMEOUT', '5')),
     'md5_file_timeout' => intval(env('SOLDER_MD5_FILE_TIMEOUT', '30')),
+    'disable_mod_api' => (bool) env('SOLDER_DISABLE_MOD_API'),
 ];
